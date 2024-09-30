8750a6992fd06a99fd89dbeaa5ee24c87583b729
João Mateus Silva <jms.lopes@discente.ufma.br>
2024-09-30 16:38:51 -0300

    Add searchSummary method to SaleService

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,7 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
+import com.devsuperior.dsmeta.projections.SummaryProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +26,25 @@ public class SaleService {
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+
+
+	public List<SaleSummaryDTO> searchSummary(String minDate, String maxDate) {
+
+		LocalDate min = LocalDate.parse(minDate);
+		LocalDate max = LocalDate.parse(maxDate);
+
+		List<SaleSummaryDTO> dtos = repository.searchSummary(min, max);
+
+		return dtos;
+
+
+	}
+
+
+
+
+
+
 
 	
 }
