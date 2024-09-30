048db7e3b20d244a3581b4edee521d08a0aabd7c
João Mateus Silva <jms.lopes@discente.ufma.br>
2024-09-30 16:38:32 -0300

    Add SaleSummaryDTO class for summarizing sales data

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,25 @@
 package com.devsuperior.dsmeta.repositories;
 
+
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) " +
+            "FROM Sale obj " +
+            "WHERE obj.date BETWEEN :minDate AND :maxDate " +
+            "GROUP BY obj.seller.name")
+    //@Query("SELECT obj FROM Sale obj JOIN FETCH obj.seller")
+    List<SaleSummaryDTO> searchSummary(@Param("minDate") LocalDate minDate, @Param("maxDate") LocalDate maxDate);
+
 
 }
