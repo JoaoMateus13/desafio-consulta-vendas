06978fe6cce087b961f331c4b42a9170d5e27efe
João Mateus Silva <jms.lopes@discente.ufma.br>
2024-10-03 14:38:51 -0300

    Add searchReport and searchSummary methods to SaleService

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.projections.SummaryProjection;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,17 +34,71 @@ public class SaleService {
 
 	public List<SaleSummaryDTO> searchSummary(String minDate, String maxDate) {
 
-		LocalDate min = LocalDate.parse(minDate);
-		LocalDate max = LocalDate.parse(maxDate);
+		if (Objects.equals(maxDate, "")) {
 
-		List<SaleSummaryDTO> dtos = repository.searchSummary(min, max);
+			LocalDate max = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
 
-		return dtos;
+			if(Objects.equals(minDate, "")) {
 
+				LocalDate min = max.minusYears(1L);
+
+				List<SaleSummaryDTO> dtos = repository.searchSummary(min, max);
+
+				return dtos;
+
+			}
+
+			LocalDate min = LocalDate.parse(minDate);
+			List<SaleSummaryDTO> dtos = repository.searchSummary(min, max);
+			return dtos;
+
+
+		}else {
+
+			LocalDate min = LocalDate.parse(minDate);
+			LocalDate max = LocalDate.parse(maxDate);
+
+			List<SaleSummaryDTO> dtos = repository.searchSummary(min, max);
+
+			return dtos;
+
+		}
+
+}
+
+	public List<SaleReportDTO> searchReport(String minDate, String maxDate, String name) {
+
+		if (Objects.equals(maxDate, "")) {
+
+			LocalDate max = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+			if(Objects.equals(minDate, "")) {
+
+				LocalDate min = max.minusYears(1L);
+
+				List<SaleReportDTO> dtos = repository.searchReport(min, max, name);
+
+				return dtos;
+
+			}
+
+			LocalDate min = LocalDate.parse(minDate);
+			List<SaleReportDTO> dtos = repository.searchReport(min, max, name);
+			return dtos;
+
+
+		}else {
+
+			LocalDate min = LocalDate.parse(minDate);
+			LocalDate max = LocalDate.parse(maxDate);
+
+			List<SaleReportDTO> dtos = repository.searchReport(min, max, name);
+
+			return dtos;
+
+		}
 
 	}
-
-
 
 
 
